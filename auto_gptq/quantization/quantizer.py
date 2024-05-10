fixed formula sym quantization

auto_gptq/quantization/quantizer.py
@@ -78,10 +78,11 @@ class Quantizer(nn.Module):
             self.scale = xmax
             self.zero = xmin
         else:
-            self.scale = (xmax - xmin) / self.maxq
             if self.sym:
+                self.scale = (xmax - xmin) / (self.maxq - 1)
                 self.zero = torch.full_like(self.scale, (self.maxq + 1) / 2)
             else:
+                self.scale = (xmax - xmin) / self.maxq
                 self.zero = torch.round(-xmin / self.scale)
 
         if self.mse: